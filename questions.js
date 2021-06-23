d35738cfd6d05224f3764e972a23d7b705615411
Ayushi9521 <79744303+Ayushi9521@users.noreply.github.com>
2021-06-23 21:04:32 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,8 @@
+var questions = [
+  ['HTML stands for', 'HighText Machine Language', 'HyperText and links Markup Language', 'HyperText Markup Language', 'None of these', '2'],
+  [' The correct sequence of HTML tags for starting a webpage is', 'Head, Title, HTML, body', 'HTML, Body, Title, Head', 'HTML, Head, Title, Body', 'Title, Head, HTML, Body', '3'],
+  ['The property in CSS used to change the background color of an element is', 'bgcolor', 'color', 'background-color', 'All of the above', '3'],
+  [' Which of the following property is used as the shorthand property for the padding properties?', 'padding-left', 'padding-right', 'padding', 'All of the above', '3'],
+  ['Which type of JavaScript language is ___', 'Object-Oriented', 'Object-Based', 'Assembly-language', 'High-level', '2'],
+  ['Which of the following function of the String object returns the character in the string starting at the specified position via the specified number of characters?', 'slice()', 'split()', 'substr()', 'search()', '3'],
+  ];
